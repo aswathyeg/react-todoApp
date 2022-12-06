e099374289ca2ac64ff0a7bd9e64f7af9666a139
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-06 18:06:30 +0530

    added submit event

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-const TodoForm = () => {
-  return <div>TodoForm</div>;
+const TodoForm = ({ todo, setTodo, todos, setTodos }) => {
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}` }, todo, ...todos]);
+      setTodo("");
+    }
+  };
+  return (
+    <div>
+      <div className="App-header">TodoApp</div>
+      <div className="form">
+        <form onClick={handleSubmit}>
+          <input onChange={handleChange} />
+          <button type="submit">Go</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default TodoForm;
