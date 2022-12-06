8420ecf54c7c08c0884070bd1d4636d337d93be5
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-06 16:58:51 +0530

    states added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <TodoForm todo={todo} setTodo={setTodo} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
 }
 
 export default App;
