ffa29b81ddb0c0cc9cb555c6bef46d232669c774
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-06 18:06:52 +0530

    added map function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const TodoList = () => {
-  return <div>TodoList</div>;
+const TodoList = ({ todos, setTodos }) => {
+  return (
+    <div>
+      {todos.map((todos) => (
+        <ul>
+          <li key={todos.id}>{todos.todo}</li>
+          <button>Edit</button>
+          <button>Delete</button>
+        </ul>
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
