83b4299e40430c6e9ea158fc0fb1bee5a109a91d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-08 23:07:06 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
-import { TodoContext } from "./components/context/Context";
+
 const App = () => {
-  const {
-    todo,
-    setTodo,
-    todos,
-
-    editId,
-
-    handleSubmit,
-    handleDelete,
-    handleEdit,
-  } = useContext(TodoContext);
-
   return (
     <div className="App">
       <div className="container">
         <h1>Todo List App</h1>
-        <TodoForm
-          handleSubmit={handleSubmit}
-          todo={todo}
-          editId={editId}
-          setTodo={setTodo}
-        />
-
-        <TodoList
-          todos={todos}
-          handleEdit={handleEdit}
-          handleDelete={handleDelete}
-        />
+        <TodoForm />
+        <TodoList />
       </div>
     </div>
   );
