4fb1b6f1b110930e34ce8e44b68b2bd5ecb6c7a9
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-08 22:48:10 +0530

    updated component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import Context from "./components/context/Context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Context>
     <App />
-  </React.StrictMode>
+  </Context>
 );
