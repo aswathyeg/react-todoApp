f1321ff15d74659dccd7dfded47220bc0e14c662
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-08 23:17:39 +0530

    deleted

--- src/components2/context/context.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from "react";
-
-const context = ({ children }) => {
-  const TodoContext = createContext();
-
-  return <TodoContext.Provider>{children}</TodoContext.Provider>;
-};
-
-export default context;
