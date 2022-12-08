b936be7d6d3c549169ca39b625cfa31984dea49b
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-08 22:55:43 +0530

    added statehandling code from App

--- src/components/context/Context.js
+++ src/components/context/Context.js
@@ -6,9 +6,51 @@ const Context = ({ children }) => {
   const [todos, setTodos] = useState([]);
   const [editId, setEditId] = useState(0);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
+  };
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
   return (
     <TodoContext.Provider
-      value={{ todo, setTodo, todos, setTodos, editId, setEditId }}
+      value={{
+        todo,
+        setTodo,
+        todos,
+        setTodos,
+        editId,
+        setEditId,
+        handleSubmit,
+        handleDelete,
+        handleEdit,
+      }}
     >
       {children}
     </TodoContext.Provider>
