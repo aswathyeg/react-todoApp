c43350a118cc149eef91891258beb297ef2ca71e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-13 17:38:06 +0530

    added devtool

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 const reducer = combineReducers({});
 const initialState = {};
@@ -6,6 +7,6 @@ const middleWare = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  applyMiddleware(...middleWare)
+  composeWithDevTools(applyMiddleware(...middleWare))
 );
 export default store;
