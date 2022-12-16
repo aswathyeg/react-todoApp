1d93664e4203ccda85d8e20ad8472cb75116c530
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-16 23:25:32 +0530

    added eventhandler

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 const App = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="App">
       <header className="App-header"></header>
       <h2>Redux Todo</h2>
 
       <div className="form">
-        <form className="interform" handleSubmit>
+        <form className="interform" onSubmit={handleSubmit}>
           <input type="text"></input>
           <button>Go</button>
         </form>
