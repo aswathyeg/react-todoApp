4727348571dfc2ce5f3bbe379c88cc16baa5adc6
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-11 15:06:37 +0530

    added Container

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,32 +1,37 @@
 import React, { useContext } from "react";
 import "../Styles.css";
+
 import { TodoContext } from "./context/Context";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Container } from "bootstrap-4-react/lib/components/layout";
 const TodoList = () => {
   const { todos, handleDelete, handleEdit } = useContext(TodoContext);
   return (
-    <ul className="allTodos">
+    <ListGroup variant="flush" className="allTodos">
       {todos.map((todos) => (
-        <li className="singleTodo">
-          <span className="todoText" key={todos.id}>
-            {todos.todo}
-          </span>
-          <button
-            onClick={() => {
-              handleEdit(todos.id);
-            }}
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => {
-              handleDelete(todos.id);
-            }}
-          >
-            Delete
-          </button>
-        </li>
+        // <li className="singleTodo">
+
+        <ListGroup.Item key={todos.id} className="singleTodo">
+          {todos.todo}
+          <Container fluid>
+            <button
+              onClick={() => {
+                handleEdit(todos.id);
+              }}
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => {
+                handleDelete(todos.id);
+              }}
+            >
+              Delete
+            </button>
+          </Container>
+        </ListGroup.Item>
       ))}
-    </ul>
+    </ListGroup>
   );
 };
 
