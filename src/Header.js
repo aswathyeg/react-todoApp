5871078fce1f945c4d41271af1a4de26ade37d0b
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-11 15:05:38 +0530

    added Nav

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,22 @@
-import { Container, Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import "./Styles.css";
+import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
 function BasicExample() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container className="header-container">
-        <Nav
-          activeKey="/home"
-          // onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-        >
+        <Navbar.Brand className="brand">Docket App</Navbar.Brand>
+        <Nav className="nav-item">
           <Nav.Item>
             <Nav.Link href="/home">Home</Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link eventKey="link-1">Calender</Nav.Link>
           </Nav.Item>
-          {/* <Nav.Item>
+        </Nav>
+        {/* <Nav.Item>
         <Nav.Link eventKey="link-2">Link</Nav.Link>
       </Nav.Item>
       <Nav.Item>
@@ -23,7 +24,6 @@ function BasicExample() {
           Disabled
         </Nav.Link> 
       </Nav.Item>*/}
-        </Nav>
       </Container>
     </Navbar>
   );
