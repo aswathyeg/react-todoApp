5eb8b51d72253dfe0db928d9292503418f5a5578
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-28 15:56:45 +0530

    clear code

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,22 @@
 import { Container } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import "./Styles.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 function BasicExample() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container className="header-container">
         <Navbar.Brand className="brand">Docket App</Navbar.Brand>
-        <Nav className="nav-item">
+        {/* <Nav className="nav-item">
           <Nav.Item>
             <Nav.Link href="/home">Home</Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link eventKey="link-1">Calender</Nav.Link>
           </Nav.Item>
-        </Nav>
+        </Nav> */}
+
         {/* <Nav.Item>
         <Nav.Link eventKey="link-2">Link</Nav.Link>
       </Nav.Item>
